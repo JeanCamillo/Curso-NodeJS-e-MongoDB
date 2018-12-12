9edd99e1561d01fae883ca1b59c52871db301d4d
Jean Camillo <jean_cmillo@hotmail.com>
2018-12-12 17:30:41 -0200

    Validação de campos utilizando o middware express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,16 +1,40 @@
 module.exports = function(application){
     application.get('/formulario_inclusao_noticia', function(req,res){
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao:{},noticia:{}});
     });
 
     application.post('/noticias/salvar', function(req,res){
-        var noticia = req.body;       
+        var noticia = req.body;  
+        
+        req.assert('titulo','O titulo é obrigatório').notEmpty();
+        req.assert('resumo','O resumo é obrigatório').notEmpty();        
+        req.assert('resumo','O resumo deve conter entre 5 e 50 caracteres').len(5,50);
+        req.assert('autor','O autor é obrigatório').notEmpty();              
+        req.assert('data_noticia').custom(isValidDate).withMessage('A data é obrigatória, e o padrão é YYYY-MM-DD');
+        req.assert('noticia','A notícia é obrigatório').notEmpty();      
 
-        var connection = application.config.dbConnection();
-        var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
-
-        NoticiasDAO.salvarNoticia(noticia, function(error, result){
-            res.redirect('/noticias');
-        });                
+        var errors = req.validationErrors();
+       
+        if(errors){
+            res.render('admin/form_add_noticia', {validacao : errors, noticia : noticia});
+            return;
+        }else{
+            
+            var connection = application.config.dbConnection();
+            var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
+    
+            NoticiasDAO.salvarNoticia(noticia, function(error, result){
+                res.redirect('/noticias');
+            });
+        }
+                
     });
+
+    function isValidDate(value) {
+        if (!value.match(/^\d{4}-\d{2}-\d{2}$/)) return false;
+        
+        const date = new Date(value);
+        if (!date.getTime()) return false;
+        return date.toISOString().slice(0, 10) === value;
+    }
 }
